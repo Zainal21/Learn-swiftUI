25b2aea4583c7de0e83f73e59244d906dd9ec027
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-03-19 23:23:11 +0700

    Initial Commit

---
+++ learn-swiftui/learn_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftuiApp.swift
+//  learn-swiftui
+//
+//  Created by Muhamadzain on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
