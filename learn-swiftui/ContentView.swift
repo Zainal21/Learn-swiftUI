58e399f1390fd216160935fc695b3eb1dfc3de4f
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-03-25 14:50:07 +0700

    feat: add simple component login form

--- learn-swiftui/ContentView.swift
+++ learn-swiftui/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //    initialize state
+    @State private var username: String = ""
+    @State private var password: String = ""
+       
+       
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+               Image(systemName: "person.circle.fill")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+                   .frame(width: 100, height: 100)
+                   .foregroundColor(.blue)
+                   .padding(.bottom, 30)
+               
+               TextField("Username", text: $username)
+                   .padding()
+                   .background(Color(UIColor.lightGray))
+                   .cornerRadius(5)
+                   .padding(.bottom, 20)
+               
+               SecureField("Password", text: $password)
+                   .padding()
+                   .background(Color(UIColor.lightGray))
+                   .cornerRadius(5)
+                   .padding(.bottom, 20)
+               
+               Button(action: {
+                   print("Username: \(username), Password: \(password)")
+               }) {
+                   Text("Login")
+                       .foregroundColor(.white)
+                       .padding()
+                       .frame(maxWidth: .infinity)
+                       .background(Color.blue)
+                       .cornerRadius(5)
+               }
+           }
+           .padding()
+           .navigationBarTitle("Login")
+     
     }
 }
 
